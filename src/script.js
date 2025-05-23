9faae857e74f55e6e96eaec4938288d6ff8f592b
Paballo-Khonothi <paballokhonothi02@gmail.com>
2025-05-23 14:28:03 +0200

    Added axios and the API details

--- src/script.js
+++ src/script.js
@@ -1,9 +1,52 @@
+function weatherRefresh(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#cityName");
+    let desctriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
+    desctriptionElement.innerHTML = response.data.condition.desctription;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
+
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day}, ${hours}:${minutes}`;
+}
+function citySearch(cityName) {
+    let apiKey = "94766eobfaf05bat0eead70d35d76438";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(weatherRefresh);
+}
+
+
+
 function weatherSearch(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#enter");
-  let cityElement = document.querySelector("#city-name");
-cityElement.innerHTML = cityInput.value;
+  
+
+    citySearch(cityInput.value);
+
 }
 
 let searchformElemet = document.querySelector("#search-form");
 searchformElemet.addEventListener("submit", weatherSearch);
+
+citySearch("Pretoria");
